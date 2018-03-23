f451f448ab8bc180d5eb631d623333b992c45ee5
Sayantan Raut <sayantan.raut2016@vitstudent.ac.in>
2018-03-23 10:32:36 +0530

    Second commit

--- first.R
+++ first.R
@@ -1,3 +1,18 @@
 #my first r file
-women
-mtcars
+#women
+#mtcars
+
+source=c('Anish','Sagar','Nihari','Bhavya','Amber','Sayantan')
+courses=c('CSE','CSE','CSE','CSE','CSE','IT')
+gender=c('M','M','F','F','M','M')
+grades=c('A','S','C','S','A','B')
+marks=floor(runif(6,50,100))
+df=data.frame(source,courses,gender,grades,marks)
+df
+df$source=as.character(df$source)
+str(df)
+df$courses=factor(df$courses)
+str(df)
+df$grades=factor(df$grades,ordered=T,levels = c('F','E','D','C','B','A','S'))
+unique(df$grades)
+str(df)
