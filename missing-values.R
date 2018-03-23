06dea57a0b02ab59957e3b8b6a4b4e3ebcbeeab7
Sayantan Raut <sayantan.raut2016@vitstudent.ac.in>
2018-03-23 14:25:59 +0530

    next commit

--- missing-values.R
+++ missing-values.R
@@ -20,3 +20,8 @@ complete.cases(sleep)
 count(sleep[complete.cases(sleep),]) #No. of complete rows
 count(sleep[!complete.cases(sleep),]) #No. of imcomplete rows
 sum(is.na(sleep$Dream))
+mean(is.na(sleep$Dream)) #No. of missing values/Total number of rows
+sum(is.na(sleep))
+colSums(is.na(sleep))
+rowSums(is.na(sleep))
+str(sleep)
